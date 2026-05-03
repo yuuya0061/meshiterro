1dbb2e085cf7ed74ce63126cdafc57bb4a4a79b3
yuuya0061 <258228254+yuuya0061@ users.noreply.github.com>
2026-05-03 14:22:04 +0000

    途中終了　ユーザー３

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create] , path_names: { new: 'sign_up' }
+  resource :session
+  resources :passwords, param: :token
   root to: "homes#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
